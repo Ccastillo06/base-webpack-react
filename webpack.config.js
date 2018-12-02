0fcd420c658e3aab658328039f41b504ea0b7902
Cristian Castillo <cristian.castillo@oneclipboard.com>
2018-12-02 19:31:28 +0100

    Support css/scss/sass

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,34 @@ const config = env => ({
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: 'file-loader',
       },
+      {
+        test: /\.(css|scss|sass)$/,
+        exclude: /\.module\.(css|scss|sass)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.module\.(css|scss|sass)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+          'sass-loader',
+        ],
+      },
     ],
   },
   resolve: {
