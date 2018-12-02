57c907da3b751721ca9eaeff4b252478c9c225b0
Cristian Castillo <cristian.castillo@oneclipboard.com>
2018-12-02 18:32:05 +0100

    Add supported browserlist and babel polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,35 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
-  },
-  resolve: {
-    extensions: [
-      '.js',
-      '.jsx',
-      '.mjs' // For Apollo library
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html'
-    })
-  ],
-  devServer: {
-    contentBase: './dist'
-  }
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js',
+            '.jsx',
+            '.mjs' // For Apollo library
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: './dist'
+    }
 }
 
 module.exports = config;
