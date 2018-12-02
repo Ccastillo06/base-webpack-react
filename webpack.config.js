bd443474d83c0e5e8848b53e2427a6866ea8dc61
Cristian Castillo <cristian.castillo@oneclipboard.com>
2018-12-02 19:23:35 +0100

    Support media loading as modules

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,21 @@ const config = env => ({
         exclude: /node_modules/,
         loader: 'babel-loader',
       },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: 'file-loader',
+      },
     ],
   },
   resolve: {
